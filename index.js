d19d2ca1bcdcc7316d1bed1413a5044574bd7abe
Phant Phant <69965001+YeyintThway13@users.noreply.github.com>
2021-05-14 19:27:52 +0630

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+const Twit = require("twit");
+const config = require("./config");
+
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, "0");
+var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+var yyyy = today.getFullYear();
+
+today = yyyy + "-" + mm + "-" + dd;
+
+const T = new Twit(config);
+
+const params = {
+  q: `#WhatsHappeningInMyanmar since:${today}`,
+  count: 100,
+};
+
+T.get("search/tweets", params, function (err, data, response) {
+  const tweets = data.statuses;
+  if (!err) {
+    for (let tweet of tweets) {
+      let retweetID = tweet.id_str;
+      T.post(
+        "statuses/retweet/:id",
+        { id: retweetID },
+        function (err, data, response) {
+          if (response) {
+            console.log(`Post retweeted with id ${retweetID}`);
+          }
+          if (err) {
+            console.log("Already Retweetes");
+          }
+        }
+      );
+    }
+  }
+});
